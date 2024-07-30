c63cf14785a30dd298f105d850052848a5e25a36
Luca <luzenobi@gmail.com>
2024-07-30 21:38:17 +0200

    First commit

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use bevy::prelude::*;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    // Camera
+    commands.spawn(Camera2dBundle::default());
+}
